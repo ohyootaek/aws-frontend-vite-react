75fd8d5b84d390f8547244d994810bf1bf998c13
오유택/PD프로젝트 <cariros@gsretail.com>
2024-11-04 14:51:27 +0900

    fix:keep alive 적용

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles/Navbar.css'
 
-const onScrollTop = () => {
-  // 페이지 최상단으로 이동
-  window.scrollTo({ top: 0, behavior: 'smooth' })
-  // 스크롤 활성화
-  document.body.style.overflow = 'auto'
-}
-
 const Navbar = ({ className }) => {
   return (
     <nav className={`navbar ${className || ''}`}>
-      <Link to='/' onClick={onScrollTop}>
+      <Link to='/'>
         Home
       </Link>
       <Link to='/login'>
