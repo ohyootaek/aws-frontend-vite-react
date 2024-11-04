ff33bbf71f5f80724bd2d70e2437c4f0c0ef47ef
오유택/PD프로젝트 <cariros@gsretail.com>
2024-11-04 14:04:56 +0900

    fix:keep alive 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
-import './App.css'
-import { KeepAlive } from 'react-activation'
+// App.tsx
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import './App.css';
+import { KeepAlive } from 'react-activation';
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/login', element: <Login /> },
-]
+];
 
 const App = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
-  const [scrolling, setScrolling] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
 
   // 클릭 이벤트 핸들러
   const handleBodyClick = () => {
-    setShowNavbar((prevShowNavbar) => !prevShowNavbar)
-  }
+    setShowNavbar((prevShowNavbar) => !prevShowNavbar);
+  };
 
   useEffect(() => {
-    // body에 클릭 이벤트 추가
-    document.body.addEventListener('click', handleBodyClick)
-
-    // 컴포넌트 언마운트 시 이벤트 제거
+    document.body.addEventListener('click', handleBodyClick);
     return () => {
-      document.body.removeEventListener('click', handleBodyClick)
-    }
-  }, [])
+      document.body.removeEventListener('click', handleBodyClick);
+    };
+  }, []);
 
   useEffect(() => {
-    let scrollTimeout
+    let scrollTimeout;
     const handleScroll = () => {
-      setShowNavbar(true)
-      setScrolling(true)
+      setShowNavbar(true);
 
-      clearTimeout(scrollTimeout)
+      clearTimeout(scrollTimeout);
       scrollTimeout = setTimeout(() => {
-        setShowNavbar(false)
-        setScrolling(false)
-      }, 1000)
-    }
+        setShowNavbar(false);
+      }, 1000);
+    };
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-      clearTimeout(scrollTimeout)
-    }
-  }, [])
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(scrollTimeout);
+    };
+  }, []);
 
   return (
-    <div>
-      <Navbar className={showNavbar ? 'navbar show' : 'navbar'} />
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={<KeepAlive>{element}</KeepAlive>} />
-        ))}
-      </Routes>
-    </div>
-  )
-}
+      <div>
+        <Navbar className={showNavbar ? 'navbar show' : 'navbar'} />
+        <Routes>
+          {routes.map(({ path, element }) => (
+              <Route
+                  key={path}
+                  path={path}
+                  element={<KeepAlive id={path}>{element}</KeepAlive>}
+              />
+          ))}
+        </Routes>
+      </div>
+  );
+};
 
-export default App
+export default App;
